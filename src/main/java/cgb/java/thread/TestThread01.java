5b78e9744b256ec0238534e507ab3c9233df732d
gyx <15731102637@163.com>
2019-07-30 14:35:00 +0800

    无

--- src/main/java/cgb/java/thread/TestThread01.java
+++ src/main/java/cgb/java/thread/TestThread01.java
@@ -3,12 +3,11 @@ package cgb.java.thread;
 public class TestThread01 {
 
 	static String content;
-	public static void main(String[] args)
-	throws Exception{
+	public static void main(String[] args) throws Exception{
 		new Thread(){
 			@Override
 			public void run() {
-			  content="helloworld";
+				content="helloworld";
 			}
 		}.start();
 		Thread.sleep(300);
